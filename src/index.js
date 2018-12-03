c2e80b666ae9e7ad52239bba0a5059277fd000d9
Polina Novokreschenova <noverianhenon@yandex.ru>
2018-12-03 18:06:05 +0300

    add greeting feature

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    const userName = readlineSync.question('May I have your name? ', {
+        defaultInput: 'anonymous'
+    });
+    return `Hello, ${userName}!`;
+};
