58ce7424c7109fd8bb1ceab6b18104fa77d6d6ef
Polina Novokreschenova <noverianhenon@yandex.ru>
2018-12-04 22:35:13 +0300

    update logical package structure

--- src/games/isEven.js
+++ src/games/isEven.js
@@ -1,35 +1,14 @@
-import readlineSync from 'readline-sync';
-import greetings from '..';
+import run from '..';
 
-const isEven = num => num % 2 === 0;
-const getRandomNumber = max => Math.floor(Math.random() * max);
-const maxRandomNumber = 40;
-const triesCount = 3;
-const user = greetings();
+const gameDescription = 'Answer "yes" if number is even, otherwise answer "no".';
 
-export default () => {
-  console.log('Answer "yes" if number is even, otherwise answer "no".\n');
-
-  const run = (count) => {
-    if (count === 0) {
-      console.log(`Congratulations, ${user}!`);
-      return;
-    }
-
-    const question = getRandomNumber(maxRandomNumber);
-    const trueAnswer = isEven(question) ? 'yes' : 'no';
-
-    console.log('Question: ', question);
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer !== trueAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
-      console.log(`Let's try again, ${user}!`);
-      return;
-    }
-    console.log('Correct!');
-    run(count - 1);
-  };
-
-  run(triesCount);
+const getRandomNumber = () => {
+  const maxRandomNumber = 40;
+  return Math.floor(Math.random() * maxRandomNumber);
 };
+
+const isEven = number => number % 2 === 0;
+
+const getAnswer = question => (isEven(question) ? 'yes' : 'no');
+
+export default () => run(gameDescription, getRandomNumber, getAnswer);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const greetings = () => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name? ', {
     defaultInput: 'anonymous',
   });
   console.log(`Hello, ${userName}!`);
   return userName;
+};
+
+const triesCount = 3;
+
+export default (gameDescription, generateQuestion, getAnswerToQuestion) => {
+  const user = greetings();
+  console.log(gameDescription, '\n');
+
+  const engine = (count) => {
+    if (count === 0) {
+      console.log(`Congratulations, ${user}!`);
+      return;
+    }
+
+    const question = generateQuestion();
+    const trueAnswer = getAnswerToQuestion(question);
+
+    console.log('Question: ', question);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== trueAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
+      console.log(`Let's try again, ${user}!`);
+      return;
+    }
+    console.log('Correct!');
+    engine(count - 1);
+  };
+
+  engine(triesCount);
 };
